3d9170e1c26f58a6d7382f35d82b9ec9b95d65be
Baldur Blöndal <baldur@student.chalmers.se>
2013-12-13 04:18:41 +0100

    a

--- tester.c
+++ tester.c
@@ -1,4 +1,5 @@
-#include "queue.c"
+#include "concurrent_queue_2locks.c"
+/* #include "queue.c" */
 #include <pthread.h>
 
 void spawnThreads(int n, int x);
@@ -6,28 +7,32 @@ void *dummy_function(void *arg);
 
 int main(void)
 {
-  int n = 2;
+  int n = 6;
   int x = 100000;
 
   srand(time(NULL));
-  
   initialize_queue();
   for (int i = 0; i < 100; i++) {
     enqueue(rand());
   }
-  
-  printf(" spawnThreads(%d, %d);\n", n, x);
+
+  struct timeval t0;
+  struct timeval t1;
+  gettimeofday(&t0, NULL);
   spawnThreads(n, x);
+  gettimeofday(&t1, NULL);
+  long elapsed = (t1.tv_sec - t0.tv_sec) * 1000000 + t1.tv_usec - t0.tv_usec;
+  printf("Elapsed time: %ld\n", elapsed);
 }
 
 void spawnThreads(int n, int x)
 {
   pthread_t threadIds[n];
   for (int i = 0; i < n; ++i) {
+    printf("Launching thread: %d\n", i);
     int *number = malloc(sizeof(*number));
     *number = x/n;
     pthread_create(&(threadIds[i]), NULL, dummy_function, (void * ) number);
-    printf("Threadid: %lu\n", threadIds[i]);
   }
 
   for (int i = 0; i < n; ++i) {
@@ -37,18 +42,14 @@ void spawnThreads(int n, int x)
 
 void *dummy_function(void *arg)
 {
-  printf("Value: %d\n", *((int *) arg));
   for (int i = 0; i < *((int *) arg); i ++) {
     double coin = ((double) rand() / (RAND_MAX));
     if (coin < 0.8) {
-      printf("Enqueue\n");
       enqueue(rand());
     }
     else {
-      printf("Dequeue\n");
       int val;
       dequeue(&val);
-      printf("Value: %d\n", val);
     }
   }
   return NULL;
